41d037933ec7d0b26f145172c784236e3ab6fe41
Victoria Buzzacchi <vbuzzacchi7@gmail.com>
2023-06-15 14:57:41 -0300

    casos de test completados

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
     has_and_belongs_to_many :exams
 
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :username, presence: true, length: { minimum: 3 }
+    validates :password, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,3 +11,36 @@ describe 'User' do
     end
   end
 end
+
+describe 'User' do
+  describe 'valid' do
+    describe 'when there is no username' do
+      it 'should be invalid' do
+        u = User.new(email: 'maria@example.com')
+        expect(u.valid?).to eq(false)
+      end
+    end
+
+    describe 'when the username is too short' do
+      it 'should be invalid' do
+        u = User.new(username: 'ab', email: 'juan@example.com', password: '1234')
+        expect(u.valid?).to eq(false)
+      end
+    end
+
+    describe 'when the password is too short' do
+      it 'should be invalid' do
+        u = User.new(username: 'ab', email: 'juan@example.com')
+        expect(u.valid?).to eq(false)
+      end
+    end
+
+    describe 'when the email domain is invalid' do
+      it 'should be invalid' do
+        u = User.new(username: 'john_doe', email: 'john@example')
+        expect(u.valid?).to eq(false)
+      end
+    end
+  end
+end
+
